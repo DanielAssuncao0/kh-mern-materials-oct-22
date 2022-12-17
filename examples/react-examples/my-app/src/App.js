523a1fbdc73f79a1b9e4efe045024c953af6dede
Kishor C <kishor1124@gmail.com>
2022-12-17 15:53:25 +0530

    react list and kesy

--- examples/react-examples/my-app/src/App.js
+++ examples/react-examples/my-app/src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import Greetings  from './Greetings';
 
 function App() {
+  let names = ["Sachin", "Mike", "Harison"];
+  let employees = [
+    {id:100, name:"Alex", salary:3500, address: {state:"KA", city:"BLR"}},
+    {id:200, name:"Bruce", salary:4500, address: {state:"KA", city:"MYS"}}
+  ];
   return (
     <div>
       <h1>Root Component</h1>
-      <Greetings />
-      <Greetings name = "Sachin" />
+      <button className = "btn btn-danger">My Button</button>
+      {names.map( (item, index) =>  <p key = {index}>Hello {item}!</p>)}
+      <div>
+        <h3>List of users</h3>
+        <ol>
+          {names.map( (item, index) => <li className = "list" key = {index}>{item}</li>)}
+        </ol>
+      </div>
+      <div>
+        <h2>List of employees</h2>
+        <table className = "table">
+          <thead>
+            <tr>
+              <th>ID</th><th>NAME</th><th>SALARY</th><th>STATE</th><th>CITY</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((item, index)=> <tr key = {index}>
+              <td>{item.id}</td><td>{item.name}</td><td>{item.salary}</td>
+              <td>{item.address.state}</td><td>{item.address.city}</td>
+            </tr>)}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
---
+++ examples/react-examples/my-app/src/Hello.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Hello extends React.Component {
+    render() {
+        return <div>
+            <h2>Hello component</h2>
+        </div>
+    }
+}
+
+//export default Hello;
