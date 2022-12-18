4b882cdff7fcf7d8d8dccae2a797d90ad43ab033
Kishor C <kishor1124@gmail.com>
2022-12-18 15:59:10 +0530

    react.js state and events

---
+++ examples/react-examples/my-app/src/Forms.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+export function Form() {
+    let [name, setName] = useState('');
+    let [gender, setGender] = useState('');
+    let [dob, setDob] = useState('');
+
+    let handleChange = (e) => {
+        let inputName = e.target.name;
+        let inputValue = e.target.value;
+       
+        if(inputName == "n1") {
+            setName(inputValue)
+        }
+        if(inputName == "n2") {
+            setGender(inputValue);
+        }
+        if(inputName == "n3") {
+            setDob(inputValue);
+        }
+    }
+    let handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    return (<div>
+        <h2>Simple Registration Form</h2>
+        <form onSubmit = {handleSubmit}>
+            <div>Enter Name <input type = "text" name = "name" 
+                autoComplete="off" onChange = {handleChange}></input></div>
+            <div>
+                Select Gender 
+                <input type = "radio" name = "gender" value = "M" 
+                onChange = {handleChange}></input>M
+                <input type = "radio" name = "gender" value = "F" 
+                onChange = {handleChange}></input>F
+            </div>
+            <div>
+                Select DOB <input type = "date" name = "dob" 
+                onChange = {handleChange}></input>
+            </div>
+            <div>
+                <input type = "submit" value = "Register" className = "btn btn-primary"></input>
+            </div>
+        </form>
+        <div>
+            Name: {name}, Gender: {gender}, Dob: {dob}
+        </div>
+    </div>)
+}
